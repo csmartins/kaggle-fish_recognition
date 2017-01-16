ae8ec90d660e0f8957a8a28a35f92f0ac1284ca9
Patricia <patriciaghiraldelli@gmail.com>
2017-01-15 22:11:02 -0200

    Arrumando arquivo detect!

--- utils/detect_fish.py
+++ utils/detect_fish.py
@@ -44,9 +44,7 @@ def rotate_image(img, angle, extra_coords):
     dst = cv2.warpAffine(img,M,(cols,rows))
     return dst, new_coords
 
-'''Funcao responsavel por gerar a piramide da imagem. A piramide consiste em varias imagens provenientes
-da original, porem com tamanho variados. Importante para quando usarmos o sliding window ser facil de reconhecer
-peixes de tamanho variados.
+'''Funcao responsavel por gerar a piramide da imagem e todas as suas rotacoes (90,180,270).
 Recebe como parametro a imagem, uma escala que define o quanto uma imagem sera menor que a anterior (iniciando
 com a imagem original) e o minSize que sera o tamanho minimo e criterio de parada.'''
 def pyramid(image, scale, minSize, extra_coords=[]):
@@ -75,7 +73,7 @@ def pyramid(image, scale, minSize, extra_coords=[]):
         yield rotate_image(image,180, extra_coords=extra_coords)
         yield rotate_image(image,270, extra_coords=extra_coords)
 
-'''Funcao que retorna as imagens da piramide da imagem parametro.'''
+'''Funcao que retorna as imagens da piramide e rotacao da imagem parametro.'''
 def image_pyramid(img, showImage):
     image = cv2.imread(img)
     if image is None:
@@ -104,82 +102,12 @@ def sliding_window(image, stepSize, windowSize):
         for x in range(0, image.shape[1] - windowSize[0], stepSize):
             # retorna largura e altura e imagem
             yield (x, y, image[y:y + windowSize[1], x:x + windowSize[0]])
-
-'''========================================= FUNCOES DE TESTE PARA VER FUNCIONAMENTO ==================================================
-                                              SERAO APAGADAS FUTURAMENTE                                                                '''
-def test_pyramid():
-    print("Pyramid of " + img_test)
-    list(image_pyramid(img_test, True))
-    
-def test_sliding():
-    image = cv2.imread(img_test)
-    if image is None:
-        raise ValueError('%s not found' % img_test)
-    image = cv2.resize(image, (256, 256)) 
-    winH= 32
-    winW = 32
-    extra_coords = [
-        np.asarray([[100, 100]])
-    ]
-    for resized, coords in pyramid(image, 0.7, (64,64), extra_coords=extra_coords):
-        for (x, y, window) in sliding_window(resized, stepSize=16, windowSize=(winW, winH)):
-            if window.shape[0] != winH or window.shape[1] != winW:
-                continue
-
-            #INSERIR METODO PARA RECONHECER PEIXE
-
-            clone = resized.copy()
-            cv2.rectangle(clone, (x, y), (x + winW, y + winH), (0, 255, 0), 2)
-            cv2.circle(clone, tuple(coords[0][0]), 4, (255,0,0), 3)
-            cv2.imshow("Window", clone)
-            cv2.waitKey(1)
-            time.sleep(0.05)
-
-"""Checa se duas boxes estao sobrepostas pelo fator factor.
-Cada box eh uma matriz com 4 linhas e 2 colunas, representando os pontos, em ordem:
-    - esquerda superior
-    - esquerda inferior
-    - direita superior
-    - direita inferior"""
-def is_overlapping(box1, box2, factor=0.8):
-    box1 = np.asarray(box1)
-    box2 = np.asarray(box2)
-
-    height1 = box1[1, 1] - box1[0, 1]
-    width1 = box1[2, 0] - box1[0, 0]
-    area1 = height1 * width1
-
-    height2 = box2[1, 1] - box2[0, 1]
-    width2 = box2[2, 0] - box2[0, 0]
-    area2 = height2 * width2
-
-    if area1 < area2:
-        bigger = area2
-    else:
-        bigger = area1
-
-    ys = [(1, box1[0, 1]), (1, box1[1, 1]),
-          (2, box2[0, 1]), (2, box2[1, 1])]
-    ys.sort(key=lambda y: y[1])
-
-    order = list(map(lambda y: y[0], ys))
-    if order == [1, 1, 2, 2] or order == [2, 2, 1, 1]:
-        return False
-    overlap_y = abs(ys[1][1] - ys[2][1])
-
-    xs = [(1, box1[0, 0]), (1, box1[2, 0]),
-          (2, box2[0, 0]), (2, box2[2, 0])]
-    xs.sort(key=lambda x: x[1])
-
-    order = list(map(lambda x: x[0], xs))
-    if order == [1, 1, 2, 2] or order == [2, 2, 1, 1]:
-        return False
-    overlap_x = abs(xs[1][1] - xs[2][1])
-
-    overlap_area = overlap_x * overlap_y
-
-    return (overlap_area / bigger) >= factor
-
+            
+'''Funcao responsavel por criar o dataset de treino a partir das sliding windows das imagens 
+provenientes do pyramid das imagens originais.
+Usa o criterio de overlapping e as anotacoes de posicoes dos peixes (no csv fish_annotation) para
+determinar se a window possui peixe ou nao. Salva todas as windows e um csv com o nome de cada uma 
+e seu resultado do overlapping com o peixe real.'''
 def collect_train_data(images_dir, positions_dir, output_dir):
     from clint.textui.progress import bar
 
@@ -267,7 +195,8 @@ def collect_train_data(images_dir, positions_dir, output_dir):
                         csv_file.write('%s,%s,%s\n' % (img_file, out_im_name, overlap))
                         cv2.imwrite(P.join(output_dir, out_im_name), window)
 
-"""retorna x e y"""
+"""Le as imagens das janelas, aplica whitening e hog e retorna as matrizes x e y
+para qualquer algoritmo de classificacao usa-las."""
 def read_process_data(images_dir, train_samples=10000, discard_false=0.9):
     from skimage.feature import hog
     from skimage import color
@@ -304,6 +233,8 @@ def read_process_data(images_dir, train_samples=10000, discard_false=0.9):
 
     return x, y
 
+'''Realiza o svm nas matrizes x e y passadas como parametro, utilizando os parametros
+C e kernel para o svm. Retorna uma matriz de confusao para os resultados obtidos.'''
 def evaluate_svm(x, y, test_split, C=1, kernel='rbf'):
     from sklearn.model_selection import train_test_split
 
@@ -318,7 +249,84 @@ def evaluate_svm(x, y, test_split, C=1, kernel='rbf'):
     from sklearn.metrics import confusion_matrix
 
     return confusion_matrix(y_valid, r)
+    
+"""Checa se duas boxes estao sobrepostas pelo fator factor do parametro.
+Cada box eh uma matriz com 4 linhas e 2 colunas, representando os pontos, em ordem:
+    - esquerda superior
+    - esquerda inferior
+    - direita superior
+    - direita inferior
+    
+Recebe como parametro box1 e box2 como sendo um array de pontos (x,y) de suas delimitacoes, 
+e factor como o fator de sobreposicao entre as duas caixas."""
+def is_overlapping(box1, box2, factor=0.8):
+    box1 = np.asarray(box1)
+    box2 = np.asarray(box2)
 
+    height1 = box1[1, 1] - box1[0, 1]
+    width1 = box1[2, 0] - box1[0, 0]
+    area1 = height1 * width1
+
+    height2 = box2[1, 1] - box2[0, 1]
+    width2 = box2[2, 0] - box2[0, 0]
+    area2 = height2 * width2
+
+    if area1 < area2:
+        bigger = area2
+    else:
+        bigger = area1
+
+    ys = [(1, box1[0, 1]), (1, box1[1, 1]),
+          (2, box2[0, 1]), (2, box2[1, 1])]
+    ys.sort(key=lambda y: y[1])
+
+    order = list(map(lambda y: y[0], ys))
+    if order == [1, 1, 2, 2] or order == [2, 2, 1, 1]:
+        return False
+    overlap_y = abs(ys[1][1] - ys[2][1])
+
+    xs = [(1, box1[0, 0]), (1, box1[2, 0]),
+          (2, box2[0, 0]), (2, box2[2, 0])]
+    xs.sort(key=lambda x: x[1])
+
+    order = list(map(lambda x: x[0], xs))
+    if order == [1, 1, 2, 2] or order == [2, 2, 1, 1]:
+        return False
+    overlap_x = abs(xs[1][1] - xs[2][1])
+
+    overlap_area = overlap_x * overlap_y
+
+    return (overlap_area / bigger) >= factor
+
+
+'''========================================= FUNCOES DE TESTE PARA VER FUNCIONAMENTO =================================================='''
+def test_pyramid():
+    print("Pyramid of " + img_test)
+    list(image_pyramid(img_test, True))
+    
+def test_sliding():
+    image = cv2.imread(img_test)
+    if image is None:
+        raise ValueError('%s not found' % img_test)
+    image = cv2.resize(image, (256, 256)) 
+    winH= 32
+    winW = 32
+    extra_coords = [
+        np.asarray([[100, 100]])
+    ]
+    for resized, coords in pyramid(image, 0.7, (64,64), extra_coords=extra_coords):
+        for (x, y, window) in sliding_window(resized, stepSize=16, windowSize=(winW, winH)):
+            if window.shape[0] != winH or window.shape[1] != winW:
+                continue
+
+            clone = resized.copy()
+            cv2.rectangle(clone, (x, y), (x + winW, y + winH), (0, 255, 0), 2)
+            cv2.circle(clone, tuple(coords[0][0]), 4, (255,0,0), 3)
+            cv2.imshow("Window", clone)
+            cv2.waitKey(1)
+            time.sleep(0.05)
+    
+'''Funcao de teste para leitura de todos os arquivos do dataset de treino.'''
 def test_read_images():
     
     images = []
